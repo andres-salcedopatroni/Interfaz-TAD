b843faafa4bcdb47e643dfc1cafe5accf5ccf76b
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-05-30 02:45:22 -0500

    VER-USUARIO

--- src/app/componentes/ver-usuario/ver-usuario.component.spec.ts
+++ src/app/componentes/ver-usuario/ver-usuario.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VerUsuarioComponent } from './ver-usuario.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('VerUsuarioComponent', () => {
   let component: VerUsuarioComponent;
@@ -8,6 +9,9 @@ describe('VerUsuarioComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ], 
       declarations: [VerUsuarioComponent]
     });
     fixture = TestBed.createComponent(VerUsuarioComponent);
