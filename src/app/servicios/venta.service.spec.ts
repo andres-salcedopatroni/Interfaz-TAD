cf646d2bfb25e8beb9b7ccf81f5eab9449cda799
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-05-30 01:31:48 -0500

    Prueba

--- src/app/servicios/venta.service.spec.ts
+++ src/app/servicios/venta.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { VentaService } from './venta.service';
 
 describe('VentaService', () => {
   let service: VentaService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], 
+    });
     service = TestBed.inject(VentaService);
   });
 
