cfd7b263edd8e5890f6177aa7b0c37bfcd20217e
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-05-30 01:47:27 -0500

    3

--- src/app/componentes/login/login.component.spec.ts
+++ src/app/componentes/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +9,10 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule], 
+      imports: [
+        HttpClientTestingModule,    
+        FormsModule
+      ], 
       declarations: [LoginComponent]
     });
     fixture = TestBed.createComponent(LoginComponent);
