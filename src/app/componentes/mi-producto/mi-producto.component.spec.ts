9427e112b8c8d10210d0dbc8358b81e25e6843ff
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-05-30 02:25:57 -0500

    mi-pro

--- src/app/componentes/mi-producto/mi-producto.component.spec.ts
+++ src/app/componentes/mi-producto/mi-producto.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MiProductoComponent } from './mi-producto.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('MiProductoComponent', () => {
   let component: MiProductoComponent;
@@ -8,6 +9,9 @@ describe('MiProductoComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ], 
       declarations: [MiProductoComponent]
     });
     fixture = TestBed.createComponent(MiProductoComponent);
