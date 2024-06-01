8e3a47032af5b1ba06dfa5ee46b75f83bd3fe0c0
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-06-01 02:03:59 -0500

    Ventana categoria

--- src/app/componentes/categoria-producto/categoria-producto.component.spec.ts
+++ src/app/componentes/categoria-producto/categoria-producto.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CategoriaProductoComponent } from './categoria-producto.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CategoriaProductoComponent', () => {
   let component: CategoriaProductoComponent;
@@ -8,6 +9,9 @@ describe('CategoriaProductoComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ], 
       declarations: [CategoriaProductoComponent]
     });
     fixture = TestBed.createComponent(CategoriaProductoComponent);
--- src/app/componentes/categoria-producto/categoria-producto.component.ts
+++ src/app/componentes/categoria-producto/categoria-producto.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductoService } from 'src/app/servicios/producto.service';
 
 @Component({
   selector: 'app-categoria-producto',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./categoria-producto.component.css']
 })
 export class CategoriaProductoComponent {
+
+  categoria: string | null;
+
+  constructor(private route: ActivatedRoute, private servicioProducto: ProductoService){
+    this.categoria = this.route.snapshot.paramMap.get('categoria');
+  }
 
 }
